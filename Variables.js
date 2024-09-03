88ed0f8479627a112c4a490abffc7a56cd9c0726
Collins1402 <collins.kibiru@strathmore.edu>
2024-09-03 13:16:41 +0300

    Modified

---
+++ Variables.js
@@ -0,0 +1,25 @@
+//Declaring variables
+function getSomething(){
+    let user_name="Collins";
+    let age=19;
+    let isStudent=true;
+
+    console.log(user_name);
+    console.log(age);
+    console.log(isStudent);
+
+}
+
+getSomething();
+
+//Converting a String
+function add(stringNum){
+    let number=stringNum;
+    return number+10;
+}
+
+let ans=add(42);
+console.log(ans);
+
+
+//Creating a null varriable
