1cf772ca18e29002e726c744286dcbf1454789f5
Collins1402 <collins.kibiru@strathmore.edu>
2024-09-03 13:51:42 +0300

    Modified

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
-//Loops 
-function getLoops(){
+// While Loops 
+function getWhileLoops(){
     let number = 1;
 
     while (number <= 20) {
@@ -8,4 +8,15 @@ function getLoops(){
 }
 
 }
-getLoops();
+getWhileLoops();
+
+// For loops
+function getForLoops(){
+    for (let i = 1; i <= 10; i++) {
+        let multiple = 3 * i;
+        console.log("The Result is:", multiple);
+}
+
+}
+
+getForLoops();
