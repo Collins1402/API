35784e3861dbee185384ce3f602faaecc5a859b6
Collins1402 <collins.kibiru@strathmore.edu>
2024-09-03 13:25:17 +0300

    Fisrt commit

---
+++ BasicArithmetic.js
@@ -0,0 +1,11 @@
+//Calculate Area
+function Area(){
+    let length = 10; 
+    let width = 5; 
+    let area = length * width;
+
+    console.log("The area of the rectangle is:", area);
+
+}
+
+Area();
