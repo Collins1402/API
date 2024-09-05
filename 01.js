6c771dcd16962b205cd04e7e02484bbf1911d4f8
Collins1402 <collins.kibiru@strathmore.edu>
2024-09-05 14:32:53 +0300

    modified

--- 01.js
+++ 01.js
@@ -1,9 +1,5 @@
 let user_name;
 
-user_name = window.prompt;{"enter your message: "};
-
-console.log;{"Hello" + user_name};
-
 document.getElementById("submit_button").onclick = function(){
     user_name = document.getElementById("myinput").ariaValueMax;
     console.log(user_name)
